2aea8f37ef868222131734fae85d5a773b5f6a56
rburketaylor <rburketaylor@gmail.com>
2021-12-14 17:54:14 -0400

    Non-working just checking other branch.

--- src/main/java/com/iamtis/DelayBeGone/DelayBeGone.java
+++ src/main/java/com/iamtis/DelayBeGone/DelayBeGone.java
@@ -6,13 +6,10 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod(DelayBeGone.MOD_ID)
 public class DelayBeGone{
     public static final String MOD_ID = "delaybegone";
-    private static final Logger LOGGER = LogManager.getLogger();
 
     public DelayBeGone(){
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
--- src/main/java/com/iamtis/DelayBeGone/Events/IsBlockingEventHandler.java
+++ src/main/java/com/iamtis/DelayBeGone/Events/IsBlockingEventHandler.java
@@ -1,6 +1,8 @@
 package com.iamtis.DelayBeGone.Events;
 
-import net.minecraft.world.item.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ShieldItem;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,7 +14,11 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 public class IsBlockingEventHandler {
     @SubscribeEvent
     public static void removeBlockingDelay(LivingEntityUseItemEvent event){
-        if(event.getItem().is(Items.SHIELD)){
+        ShieldItem shield = new ShieldItem(new Item.Properties());
+        System.out.println(event.getItem());
+        System.out.println(shield);
+
+        if(event.getItem().getItem().equals(shield)){
             event.setDuration(100);
         }
     }
